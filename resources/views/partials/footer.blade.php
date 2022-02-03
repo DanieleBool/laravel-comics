c301b57c9de57aa247c86f4c5ce1e4907e81d514
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2022-02-03 15:22:58 +0100

    aggiunta footer e stile

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,126 @@
+<footer>
+    <!-- footer top -->
+        <div class="top container">
+            <div class="container">
+                <ul>
+                    <li>
+                        <img id="digital" src="../assets/img/buy-comics-digital-comics.png" alt="">
+                        <a href="#">Digital comics</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                        <a href="#">Dc merchandise</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        <a href="#">Subscription</a>
+                    </li>
+                    <li>
+                        <img id="locator" src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        <a href="#">Comic shop location</a>
+                    </li>
+                    <li>
+                        <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        <a href="#">Dc power visa</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- /footer top -->
+
+        <!-- footer center -->
+        <div class="middle">
+            <div class="container">
+                <div class="links">
+                    <div class="col">
+                        <ul>
+                            <li><h3>dc comics</h3></li>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li> 
+                        </ul>
+                        <ul>
+                            <li><h3>shop</h3></li>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <ul>
+                            <li><h3>dc</h3></li>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent</a></li>
+                            <li><a href="#">characters</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <ul>
+                            <li><h3>sites</h3></li>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /footer center -->
+
+        <!-- footer bottom -->
+        <div class="bottom">
+            <div class="container">
+                <div class="divider">
+                    <div class="sign-up">
+                        <button>Sign-up now!</button>
+                    </div>
+                    <div class="social">
+                        <div class="follow">
+                            <p>Follow us</p>
+                        </div>
+                        <div class="icons">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-facebook.png" alt="facebook">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-twitter.png" alt="twitter">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-youtube.png" alt="youtube">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-pinterest.png" alt="pinterest">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-periscope.png" alt="periscope">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- /footer bottom -->
+</footer>
--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,14 +1,16 @@
-<section>
-<h2>Current series</h2>
-<div class="box">
-    @foreach($cards as $card)  
-    <div class="card">
-        <img src="{{$card['thumb']}}" :alt="card.series">
-        <h3>{{$card['title']}}</h3>
-    </div>
-    @endforeach
-</div>
-<div class="load">
-    <button>load more</button>
-</div>
-</section>
+<main class="dark">
+    <section class="container">
+        <h2>Current series</h2>
+        <div class="box">
+            @foreach($cards as $card)  
+            <div class="card">
+                <img src="{{$card['thumb']}}" :alt="card.series">
+                <h3>{{$card['title']}}</h3>
+            </div>
+            @endforeach
+        </div>
+        <div class="load">
+            <button>load more</button>
+        </div>
+    </section>
+</main>
